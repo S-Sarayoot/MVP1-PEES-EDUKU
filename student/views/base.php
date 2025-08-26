af60389048a863f4e8fcb2e5a885c71f03e9c177
Siripoom Limsoontorn <65209010011@thonburi.ac.th>
2025-08-26 11:42:46 +0700

    feat: add personal information form and update title in base layout

--- student/views/base.php
+++ student/views/base.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-  <title><?= htmlspecialchars($title ?? 'Untitled') ?></title>
+  <title>EDU KU - <?= htmlspecialchars($title ?? 'Untitled') ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   </head>
