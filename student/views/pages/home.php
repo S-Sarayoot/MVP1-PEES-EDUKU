c371fe53fb02815a00093b5ae65c789ce181f4b2
Siripoom Limsoontorn <65209010011@thonburi.ac.th>
2025-08-29 20:32:33 +0700

    ✨ feat: add workshop list component with dynamic content and styling

--- student/views/pages/home.php
+++ student/views/pages/home.php
@@ -22,6 +22,30 @@ component('components/MenuCarousel.php', [
             'icon' => ''
         ],
         [
+            'label' => 'คลิปวิดีโอ',
+            'description' => 'คลังวิดีโอ',
+            'link' => '/video',
+            'icon' => '/uploads/play-icon.png'
+        ],
+        [
+            'label' => '3 คำ',
+            'description' => 'Script',
+            'link' => '/script',
+            'icon' => ''
+        ],
+        [
+            'label' => 'คลิปวิดีโอ',
+            'description' => 'คลังวิดีโอ',
+            'link' => '/video',
+            'icon' => '/uploads/play-icon.png'
+        ],
+        [
+            'label' => '3 คำ',
+            'description' => 'Script',
+            'link' => '/script',
+            'icon' => ''
+        ],
+        [
             'label' => 'Quik 3s',
             'description' => 'Exam',
             'link' => '/exam',
@@ -38,7 +62,95 @@ component('components/MenuCarousel.php', [
             'description' => 'Story board',
             'link' => '/storyboard',
             'icon' => '/uploads/storyboard-icon.png'
-        ]
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+        [
+            'label' => 'Story board',
+            'description' => 'Story board',
+            'link' => '/storyboard',
+            'icon' => '/uploads/storyboard-icon.png'
+        ],
+
+
     ]
 ]);
+
+component('components/workshop-list.php', [
+  'title' => 'บทเรียน',
+  'instruction_title' => 'คำชี้แจง',
+  'instruction_text'  => 'คำชี้แจง workshop รวม…',
+  'workshops' => [
+    [
+      'name' => 'Workshop 1',
+      'desc' => 'คำชี้แจงเกี่ยวกับ workshop 1.. เวลา 3 ชั่วโมง',
+      'buttons' => [
+        ['label' => 'กิจกรรม',  'href' => '/workshop/1/activity'],
+        ['label' => 'ทรัพยากร', 'href' => '/workshop/1/resources'],
+        ['label' => 'สะท้อนคิด', 'href' => '/workshop/1/reflection'],
+      ],
+    ],
+    [
+      'name' => 'Workshop 2',
+      'desc' => 'คำชี้แจง workshop 2.. เวลา 3 ชั่วโมง',
+    ],
+    [
+      'name' => 'Workshop 3',
+      'desc' => 'คำชี้แจง workshop 3.. เวลา 3 ชั่วโมง',
+    ],
+  ],
+]);
+
 ?>
